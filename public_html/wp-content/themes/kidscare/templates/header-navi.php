951939cc2cbcd458ee43f4409000c83ddd2c2e21
waroxa <waroxa2@gmail.com>
2025-08-16 10:31:06 -0400

    Refactor navigation markup and add custom walker

--- public_html/wp-content/themes/kidscare/templates/header-navi.php
+++ public_html/wp-content/themes/kidscare/templates/header-navi.php
@@ -27,27 +27,20 @@
 				</div>
 			</div><div class="sc_layouts_column sc_layouts_column_align_right sc_layouts_column_icons_position_left sc_layouts_column_fluid column-3_4">
 				<div class="sc_layouts_item">
-					<?php
-					// Main menu
-					$kidscare_menu_main = kidscare_get_nav_menu( 'menu_main' );
-					// Show any menu if no menu selected in the location 'menu_main'
-					if ( kidscare_get_theme_setting( 'autoselect_menu' ) && empty( $kidscare_menu_main ) ) {
-						$kidscare_menu_main = kidscare_get_nav_menu();
-					}
-					kidscare_show_layout(
-						$kidscare_menu_main,
-						'<nav class="menu_main_nav_area sc_layouts_menu sc_layouts_menu_default sc_layouts_hide_on_mobile"'
-							. ' itemscope itemtype="//schema.org/SiteNavigationElement"'
-							. '>',
-						'</nav>'
-					);
-					// Mobile menu button
-					?>
-					<div class="sc_layouts_iconed_text sc_layouts_menu_mobile_button">
-						<a class="sc_layouts_item_link sc_layouts_iconed_text_link" href="#">
-							<span class="sc_layouts_item_icon sc_layouts_iconed_text_icon trx_addons_icon-menu"></span>
-						</a>
-					</div>
+<?php
+// Main menu
+?>
+<nav class="main-nav" itemscope itemtype="//schema.org/SiteNavigationElement">
+<?php
+wp_nav_menu(
+array(
+'theme_location' => 'menu_main',
+'container'      => false,
+'walker'         => class_exists( 'Kidscare_Main_Nav_Walker' ) ? new Kidscare_Main_Nav_Walker() : '',
+)
+);
+?>
+</nav>
 				</div>
 				<?php
 				if ( kidscare_exists_trx_addons() ) {
