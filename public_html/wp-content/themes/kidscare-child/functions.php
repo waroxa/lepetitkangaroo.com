211ffff589a3a60dca4c5a68c3e507077ed30d1c
waroxa <waroxa@gmail.com>
2025-08-18 11:12:39 -0400

    restoring menu

--- public_html/wp-content/themes/kidscare-child/functions.php
+++ public_html/wp-content/themes/kidscare-child/functions.php
@@ -4,20 +4,9 @@
  */
 
 function kidscare_child_scripts() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
-    wp_enqueue_script( 'desktop-header', get_stylesheet_directory_uri() . '/js/desktop-header.js', array(), null, true );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri(). '/style.css' );
 }
 
-add_action( 'wp_enqueue_scripts', 'kidscare_child_scripts' );
-
-function kidscare_child_disable_menu_fallback() {
-    wp_dequeue_script( 'kidscare-init' );
-    wp_deregister_script( 'kidscare-init' );
-    wp_dequeue_script( 'superfish' );
-    wp_deregister_script( 'superfish' );
-}
-
-add_action( 'wp_enqueue_scripts', 'kidscare_child_disable_menu_fallback', 1001 );
+add_filter('wp_enqueue_scripts', 'kidscare_child_scripts');
 
 ?>
